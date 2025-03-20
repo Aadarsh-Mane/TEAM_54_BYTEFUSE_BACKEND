927ef0ee4c5235ff3f880b15d41c6fc0502ba8e1
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-03-20 17:28:30 +0530

    initialize project with Express server and basic configuration

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const port = 1000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for URL-encoded bodies
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Welcome to WomenSafety ! v2 ❤️");
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
