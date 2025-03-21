e932c2ba0d19422abdbc234a938bfa2a08d9a292
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-03-21 08:09:11 +0530

    add database connection and authentication middleware

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import { connectDB } from "./dbConnect.js";
 
 const port = 1000;
 
 const app = express();
 const server = http.createServer(app);
-
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for URL-encoded bodies
 app.use(cors());
